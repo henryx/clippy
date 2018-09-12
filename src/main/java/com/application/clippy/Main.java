f7ee604a67d4540efa4a28cb7fcd0302a93ace4c
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-09-12 23:35:09 +0200

    Added main class

---
+++ src/main/java/com/application/clippy/Main.java
@@ -0,0 +1,14 @@
+/*
+  Copyright (C) 2018 Enrico Bianchi (enrico.bianchi@gmail.com)
+  Project       clippy
+  Description   A simple clipboard manager
+  License       GPL version 2 (see LICENSE for details)
+ */
+
+package com.application.clippy;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
