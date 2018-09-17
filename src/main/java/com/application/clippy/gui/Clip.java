bafc5b9dad0eea6e4d85de9dbcbadcafc4f70be7
Enrico Bianchi <enrico.bianchi@gmail.com>
2018-09-17 23:21:02 +0200

    Added class for manage clipboard

---
+++ src/main/java/com/application/clippy/gui/Clip.java
@@ -0,0 +1,32 @@
+/*
+  Copyright (C) 2018 Enrico Bianchi (enrico.bianchi@gmail.com)
+  Project       clippy
+  Description   A simple clipboard manager
+  License       GPL version 2 (see LICENSE for details)
+ */
+
+package com.application.clippy.gui;
+
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+
+public class Clip implements Runnable {
+    private Clipboard clipboard;
+
+    public Clip() {
+        this.clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(10000);
+
+                this.clipboard.setContents(new StringSelection(""), null);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+}
